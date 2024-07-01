0e5b1866d33758056232821b7e5f0b19a64eeebf
firdous alam <firdous.alam2058@gmail.com>
2024-07-01 14:02:50 +0530

    Code Update

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,38 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): any {
-    let data =[
-      {
-        userId: 1,
-        "id": 1,
-        "title": "delectus aut autem",
-        "completed": false
-      },
-      {
-        "userId": 1,
-        "id": 2,
-        "title": "quis ut nam facilis et officia qui",
-        "completed": false
-      },
-      {
-        "userId": 1,
-        "id": 3,
-        "title": "fugiat veniam minus",
-        "completed": false
-      },
-      {
-        "userId": 1,
-        "id": 4,
-        "title": "et porro tempora",
-        "completed": true
-      },
-      {
-        "userId": 1,
-        "id": 5,
-        "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
-        "completed": false
-      }];
-    return data;
+  getHello(): string {
+    return 'Hello World!';
   }
 }
--- src/registration/registration.module.ts
+++
@@ -1,9 +0,0 @@
-import { RegistrationController } from './registration.controller';
-import { RegistrationService } from './registration.service';
-import { Module } from '@nestjs/common';
-@Module({
-  imports: [],
-  controllers: [RegistrationController],
-  providers: [RegistrationService],
-})
-export class RegistrationModule {}
