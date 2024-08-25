5ffe3f95527752afc1190eca102b45d88c813fe5
firdous alam <firdous.alam2058@gmail.com>
2024-08-25 13:02:30 +0530

    updated main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config'; 
+//import { ConfigService } from '@nestjs/config'; 
 const PORT = process.env.PORT || 4000
 async function bootstrap() {
   
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
- // const port = configService.get('port');
-  await app.listen(port);
+//  const configService = app.get(ConfigService);
+ // const PORT = configService.get('port');
+  await app.listen(PORT);
 }
 bootstrap();
